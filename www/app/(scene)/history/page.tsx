8467bbb2cf64b47205f2cfc12fc88addfac0d34e
Julian S. <julian.schmidt1005@gmail.com>
2025-09-15 16:48:32 +0200

    feat: add history page

---
+++ www/app/(scene)/history/page.tsx
@@ -0,0 +1,9 @@
+import PersonalScenes from '@/components/personal-scenes';
+
+export default async function Page() {
+    return (
+        <div className='relative bg-background/70 mt-16'>
+            <PersonalScenes />
+        </div>
+    );
+}
