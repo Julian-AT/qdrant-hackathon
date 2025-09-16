8c308a70412dbf33f691825baaeb870156c50f5d
Julian S. <julian.schmidt1005@gmail.com>
2025-09-16 17:56:54 +0200

    chore: delete model

--- model/interior-panorama/scripts/dataset.ts
+++
@@ -1,7 +0,0 @@
-/*
-Script to create a dataset for the interior panorama generation model
-This scripts builds a dataset of interior panorama images from the /assets directory and pushes it to the huggingface hub
-*/
-
-
-
