4ecfe8b4a91bd20b6c74d3b86de869efb6878d14
Julian S. <julian.schmidt1005@gmail.com>
2025-09-16 21:51:56 +0200

    feat: add animation for gradient background

--- www/components/gradient-background.tsx
+++ www/components/gradient-background.tsx
@@ -15,6 +15,9 @@ const GradientBackground = () => {
         <div className="absolute inset-0 pointer-events-none z-[1] overflow-x-hidden max-w-screen h-full">
             <motion.div
                 className="absolute left-1/2 -translate-x-1/2 top-[calc(1250px)] border border-blue-500 aspect-square"
+                initial={{ y: 200, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 1.5, ease: "easeOut" }}
             >
                 <div className="w-[125rem] h-[125rem] rounded-full bg-gradient-to-t from-pink-500 to-sky-500/90 blur-[100px] absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2"></div>
                 <div className="w-[100rem] h-[100rem] rounded-full bg-gradient-to-t from-purple-600 to-orange-500/90 blur-[70px] absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2"></div>
