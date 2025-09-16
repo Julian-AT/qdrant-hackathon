4f9e63f6937daa718a37a1ec6ddb5bb091143c8c
Julian S. <julian.schmidt1005@gmail.com>
2025-09-16 21:47:51 +0200

    chore: set correct og image size

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -37,8 +37,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
+        width: 1920,
+        height: 1080,
         alt: siteConfig.name,
       },
     ],
